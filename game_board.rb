c5ba4f2b20afb7dfc2ffdcff0f2c991b21d81ad1
Erica Ford <erica.ford0205@gmail.com>
2024-07-30 15:25:10 -0500

    Fix variable @@board and end comments

--- game_board.rb
+++ game_board.rb
@@ -1,15 +1,14 @@
 module GameBoard
   class Board
 
-    # Constructor 
-    def initialize(board)
-      @board = board
+    @@board = Hash.new
 
+    # Constructor 
+    def initialize()
       # Generating the board as a new obj from the class
-      @board = Hash.new
         for i in 0..2
           for j in 0..2
-            @board["#{i}#{j}"] = nil
+            @@board["#{i}#{j}"] = nil
           end
         end
     
@@ -24,7 +23,7 @@ module GameBoard
         end
       end
 
-    # end method
+    # End method
     end
 
     # Set the game board with coordinates for player choices (Setter)
@@ -34,19 +33,19 @@ module GameBoard
       # Player 1
       if player == 1
         # If there is an empty space on the board set the game piece else do nothing
-        ( @board["#{x}#{y}"] == nil ) ? piece_set = true : nil
-        ( @board["#{x}#{y}"] == nil ) ? ( @board["#{x}#{y}"] = "X" ) : nil
+        ( @@board["#{x}#{y}"] == nil ) ? piece_set = true : nil
+        ( @@board["#{x}#{y}"] == nil ) ? ( @@board["#{x}#{y}"] = "X" ) : nil
         
       # Player 2
       else
         # If there is an empty space on the board set the game piece else do nothing
-        ( @board["#{x}#{y}"] == nil ) ? piece_set = true : nil
-        ( @board["#{x}#{y}"] == nil ) ? ( @board["#{x}#{y}"] = "O" ) : nil
+        ( @@board["#{x}#{y}"] == nil ) ? piece_set = true : nil
+        ( @@board["#{x}#{y}"] == nil ) ? ( @@board["#{x}#{y}"] = "O" ) : nil
 
       end
 
       return piece_set
-    # end method
+    # End method
     end
 
     # Draw the Game board (Getter)
@@ -54,14 +53,14 @@ module GameBoard
       for i in 0..2
         for j in 0..2
           if j != 2
-            ( @board["#{i}#{j}"] == nil ) ? ( print "|   " ) : ( print "| #{@board["#{i}#{j}"]} ")
+            ( @@board["#{i}#{j}"] == nil ) ? ( print "|   " ) : ( print "| #{@@board["#{i}#{j}"]} ")
           else
-            ( @board["#{i}#{j}"] == nil ) ? ( print "|   | \n" ) : ( print "| #{@board["#{i}#{j}"]} |")
+            ( @@board["#{i}#{j}"] == nil ) ? ( print "|   | \n" ) : ( print "| #{@@board["#{i}#{j}"]} |")
           end
         end
       end
 
-    # end method
+    # End method
     end 
   
   # End class
