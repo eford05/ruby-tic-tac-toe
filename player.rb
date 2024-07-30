601a02e5b6f39f92f3ad06dfa3058f3f2c873221
Erica Ford <erica.ford0205@gmail.com>
2024-07-30 15:41:37 -0500

    Begin writing Player class

--- player.rb
+++ player.rb
@@ -1,2 +1,18 @@
 module Player
+  class Player
+
+    attr_reader :player_moves
+
+    @@player_moves = Array.new
+
+    # Push the player's moves to the player moves array
+    def move (x, y)
+      player_moves.push("#{x}#{y}")
+    
+    # End method
+    end
+
+  # End class
+  end
+# End module
 end
