faa86596dbef1a6d81131d9a86f44474760c463c
Erica Ford <erica.ford0205@gmail.com>
2024-07-30 15:25:53 -0500

    Test GameBoard module

--- main.rb
+++ main.rb
@@ -3,14 +3,15 @@ include GameBoard
 
 # Run Game Logic 
 def game_start
-  prev_board = GameBoard::Board.new
-  game_board = GameBoard::Board.new.generate_board
+
+  # Instantiate The Board
+  board = GameBoard::Board.new
 
   puts "Directions: Take turns placing a marker on one of the coordinates. The first player to get 3 in a row wins. \n"
-  prev_board.preview_board
+  board.preview_board
   puts ""
 
-  print game_board
+  board.draw_board
 
   # Play game until there is a winner or there are no more turns
 end
