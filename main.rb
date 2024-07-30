3dacfeca5b30d7c0b0caedab41bc1c157433f2d7
Erica Ford <erica.ford0205@gmail.com>
2024-07-30 17:03:44 -0500

    Test array flatten idea

--- main.rb
+++ main.rb
@@ -1,11 +1,14 @@
 require_relative "game_board"
+require_relative "game_score"
 include GameBoard
+include GameScore
 
 # Run Game Logic 
-def game_start
+def play_game
 
   # Instantiate The Board
   board = GameBoard::Board.new
+  win = GameScore::Score.new
 
   puts "Directions: Take turns placing a marker on one of the coordinates. The first player to get 3 in a row wins. \n"
   board.preview_board
@@ -14,6 +17,11 @@ def game_start
   board.draw_board
 
   # Play game until there is a winner or there are no more turns
+
+  arr = ["00", "01", "02"]
+  # Test game win
+
+  win.game_win( arr )
 end
 
-game_start
+play_game
